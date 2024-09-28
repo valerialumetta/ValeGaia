a4d440d43c7b1ca02328eeae8d0cac0cc3ea0027
ggaiaa <159471855+ggaiaa@users.noreply.github.com>
2024-09-28 16:42:50 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from './views/HomePage.vue';
-import ShoeCard from './components/ShoeCard.vue'; // Importa il componente ShoeCard
+import { createRouter, createWebHistory } from 'vue-router'; //importa il router
+import HomePage from './views/HomePage.vue'; //importa la pagina HomePage.vue
+import store from './store/index'; // Importa lo store per vuex
 
-
-// Definizione delle rotte
 const routes = [
-  { path: '/', component: HomePage },
-  { path: '/shoes', component: ShoeCard }, // Rotta per la lista delle scarpe
+  { 
+    path: "/",
+    name: "Home Page",
+    component: HomePage,
+  },
 ];
 
-// Creazione del router
 const router = createRouter({
   history: createWebHistory(),
   routes
 });
 
-// Creazione e montaggio dell'app
 const app = createApp(App);
-app.use(router);
+
+app.use(router); //usa il router importato
+app.use(store); // Usa lo store importato
 app.mount('#app');
